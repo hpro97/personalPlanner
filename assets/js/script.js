bf710d236848784378ff4c3bcd9ece0f0f58754a
Hpro97 <haxmo25@gmail.com>
2024-01-19 16:57:02 +0000

    updated plan of attack

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,11 +18,17 @@
 //header set date for top of page
     //function for setting time and day
         //appends date to current day ID paragraph
+
 //code for creating and appending timeblocks for hours
     //function sets class for if is past, present, or future using dayjs
         //criteria for color change is past, present, future
-    //click event for each block when clicked can write
-    //click event for save button when clicked can save text in block to local storage
+
+//code for retrieving local storage of text in timeblocks
+    //makes text in block appear from local storage
+
+//click event for each block when clicked can write
+
+//click event for save button when clicked can save text in block to local storage
 
 // ---------------------------------//
 // ---------------------------------//
