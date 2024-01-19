5a282d19f093f452f576e52fc0b372504af081e3
Hpro97 <haxmo25@gmail.com>
2024-01-19 17:00:20 +0000

    added methodology for structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,19 +15,24 @@
 
 //plan of attack:
 
+//*use dayjs for this*
 //header set date for top of page
     //function for setting time and day
         //appends date to current day ID paragraph
 
+//*use jquery for this*
 //code for creating and appending timeblocks for hours
     //function sets class for if is past, present, or future using dayjs
         //criteria for color change is past, present, future
 
+//*use local storage get for this*
 //code for retrieving local storage of text in timeblocks
     //makes text in block appear from local storage
 
+//*use jquery for this*
 //click event for each block when clicked can write
 
+//*use local storage get and set for this*
 //click event for save button when clicked can save text in block to local storage
 
 // ---------------------------------//
